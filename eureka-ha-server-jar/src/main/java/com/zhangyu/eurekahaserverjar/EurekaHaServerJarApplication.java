e1efcd258c27e7ee3102568436a0fa6552f811d4
zhangyu <zhangyu@xingheng.ai>
2018-08-31 16:50:35 +0800

    final

---
+++ eureka-ha-server-jar/src/main/java/com/zhangyu/eurekahaserverjar/EurekaHaServerJarApplication.java
@@ -0,0 +1,12 @@
+package com.zhangyu.eurekahaserverjar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaHaServerJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaHaServerJarApplication.class, args);
+	}
+}
